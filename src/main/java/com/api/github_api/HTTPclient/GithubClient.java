6545b2644627586d3b360de2f7074143f40ed379
Breno <brenovmsilva82@gmail.com>
2024-04-27 15:06:20 -0300

    Implementing API Authentication Logic

--- src/main/java/com/api/github_api/HTTPclient/GithubClient.java
+++ src/main/java/com/api/github_api/HTTPclient/GithubClient.java
@@ -11,5 +11,5 @@ public class GithubClient {
     @GetExchange("/user/repos")
     public List<RepositoryResponse> listRepos(@RequestHeader("Authorization") String token,
                                               @RequestHeader(value = "X-GitHub-Api-Version", defaultValue = "2022-11-28") String apiVersion,
-                                              @RequestParam("visibility") String visibility);
+                                              @RequestParam("visibility") String visibility){return null;}
 }
--- src/main/java/com/api/github_api/controller/GithubController.java
+++ src/main/java/com/api/github_api/controller/GithubController.java
@@ -3,6 +3,7 @@ package com.api.github_api.controller;
 import com.api.github_api.HTTPclient.GithubClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,13 @@ public class GithubController {
     }
 
     @GetMapping("/repos")
-    public ResponseEntity<List<RepositoryResponse>> ListRepos() {
-        return null;
+    public ResponseEntity<List<RepositoryResponse>> ListRepos(@RequestHeader("token") String token) {
+        var repos = githubClient.listRepos(
+                "Bearer" + token,
+                "null",
+                "all");
+
+        return ResponseEntity.ok(repos);
     }
 }
 
