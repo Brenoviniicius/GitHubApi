8bc853a901daea010b377218481ee9ffceaa3762
Breno <brenovmsilva82@gmail.com>
2024-04-27 09:59:05 -0300

    Controller configuration

---
+++ src/main/java/com/api/github_api/controller/GithubController.java
@@ -0,0 +1,19 @@
+package com.api.github_api.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+@RestController
+@RequestMapping("/v1")
+public class GithubController {
+
+    @GetMapping("/repos")
+    public ResponseEntity<List<RespositoryResponse>> ListRepos() {
+        return null;
+    }
+}
+
+/**Mapeamento do controller da aplicação */
